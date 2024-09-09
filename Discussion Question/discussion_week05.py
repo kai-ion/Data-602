2bfd946c1dbfdbc57b3be8abc4b0302586deb1e7
kaiion <kaiion0509@gmail.com>
2024-09-09 01:39:27 -0400

    discussion_week05

--- Discussion Question/discussion_week05.py
+++ Discussion Question/discussion_week05.py
@@ -1,8 +1,30 @@
+"""Sample output
+###Q1 StringReverser###
+Original String: hello .py
+Reversed String: .py hello
+
+
+
+
+###Q2 Circle###
+Circle with radius 5:
+Area: 78.54
+Perimeter: 31.42
+"""
+
 ###------------------------------------------------------------------------------------------------------
 # Q1: Write a Python class to reverse a string word by word.
 # Example:
 # Input string : 'hello .py'
 # Expected Output : '.py hello'
+class StringReverser:
+    def __init__(self, inputString):
+        self.inputString = inputString
+
+    def reverse_words(self):
+        words = self.inputString.split()
+        reversedWords = ' '.join(reversed(words))
+        return reversedWords
 
 ###------------------------------------------------------------------------------------------------------    
 # Q2: Write a Python class named Circle constructed by a radius and two methods 
@@ -21,6 +43,14 @@ class Circle:
 ###------------------------------------------------------------------------------------------------------    
 
 def main():
+    # Q1 StringReverser
+    print("###Q1 StringReverser###")
+    inputString = 'hello .py'
+    reverser = StringReverser(inputString)
+    print("Original String:", inputString)
+    print("Reversed String:", reverser.reverse_words())
+    print("\n\n\n")
+    
     # Q2 Circle
     print("###Q2 Circle###")
     radius = 5
