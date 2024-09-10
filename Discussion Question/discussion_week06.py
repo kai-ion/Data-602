50465c7827457b0cf09172cb1a21c0f45c84bdac
kaiion <kaiion0509@gmail.com>
2024-09-09 23:08:26 -0400

    fiex mistakes

--- Discussion Question/discussion_week06.py
+++ Discussion Question/discussion_week06.py
@@ -60,7 +60,7 @@ multi-dimensional array
 """
 def ndarray_example():
     print("multi-dimensional array\n")
-    arr = np.array([1, 2, 3, 4])
+    arr = np.array([[1, 2, 3], [4, 5, 6]])
     return arr
 
 ###------------------------------------------------------------------------------------------------------  
@@ -76,14 +76,46 @@ def create_array():
 #    array1 = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 def odd_numbers():
     array1 = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-    odd = array1[array1 % 2 == 1]
-    return odd
+    print("Original array:", array1)
+    odd_numbers = array1[array1 % 2 == 1]
+    return odd_numbers
+
+###------------------------------------------------------------------------------------------------------ 
+# 5. Get the common items between a and b.
+#    #input
+#    a = np.array([1, 2, 3, 2, 3, 4, 3, 4, 5, 6])
+#    b = np.array([7, 2, 10, 2, 7, 4, 9, 4, 9, 8])
+#    #Desired Output:
+#    array([2, 4])
+def common_items():
+    a = np.array([1, 2, 3, 2, 3, 4, 3, 4, 5, 6])
+    b = np.array([7, 2, 10, 2, 7, 4, 9, 4, 9, 8])
+    print("Array a:", a)
+    print("Array b:", b)
+    common_items = np.intersect1d(a, b)
+    return common_items
 
 ###------------------------------------------------------------------------------------------------------  
+# 6. From array a remove all items present in array b.
+#    a = np.array([1, 2, 3, 4, 5])
+#    b = np.array([5, 6, 7, 8, 9])
+def remove_items():
+    a = np.array([1, 2, 3, 4, 5])
+    b = np.array([5, 6, 7, 8, 9])
+    print("Array a:", a)
+    print("Array b:", b)
+    result = np.setdiff1d(a, b)
+    return result
 
 ###------------------------------------------------------------------------------------------------------  
-
-###------------------------------------------------------------------------------------------------------  
+# 7. Find out if iris has any missing values.
+#    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+#    iris = np.genfromtxt(url, delimiter=',', dtype='float', usecols=[0,1,2,3])
+def missing_values():
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+    iris = np.genfromtxt(url, delimiter=',', dtype='float', usecols=[0,1,2,3])
+    missing_values = np.isnan(iris).sum()
+    return missing_values
 
 ###------------------------------------------------------------------------------------------------------  
 
@@ -105,7 +137,22 @@ def main():
     
     # Q4 Odd Num
     print("###Q4### Extract all odd numbers from array1.")
-    print(odd_numbers())
+    print("Odd numbers:", odd_numbers())
+    print("\n")
+    
+    # Q5 Common Items
+    print("###Q5### Get the common items between a and b.")
+    print("Common items:", common_items())
+    print("\n")
+    
+    # Q6 Remove Items
+    print("###Q6### From array a remove all items present in array b.")
+    print("Array after removing items present in b:", remove_items())
+    print("\n")
+    
+    # Q7 Missing Values
+    print("###Q7### Find out if iris has any missing values.")
+    print("Number of missing values in the iris dataset:", missing_values())
     print("\n")
 
 if __name__ == "__main__":
