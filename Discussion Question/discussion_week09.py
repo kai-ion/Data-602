72cf51d4ca8c5dd234f31bee632e350a9fa8f799
kaiion <kaiion0509@gmail.com>
2024-09-17 11:11:42 -0400

    discussion week 9

---
+++ Discussion Question/discussion_week09.py
@@ -0,0 +1,40 @@
+"""Questions
+1. Share a link to the dataset you are planning to use for your final project/proposal.
+2. Why is this dataset interesting?  Why do you want to analyze it?
+3. Display some summary statistics about your dataset.
+If you notice other classmates with similar datasets or end goals, you might consider grouping with them to complete the final project.   
+"""
+
+"""Sample output
+"""
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q1. Share a link to the dataset you are planning to use for your final project/proposal.
+
+https://www.kaggle.com/datasets/philiphyde1/nfl-stats-1999-2022
+"""
+
+
+###------------------------------------------------------------------------------------------------------  
+
+def main():
+
+    
+    # Q1 delete data
+    print("###Q1### Share a link to the dataset you are planning to use for your final project/proposal.")
+    
+    print("\n")
+    
+    
+
+if __name__ == "__main__":
+    main()
+
+"""
+*    *
+*  *
+* *
+* *
+*  *
+*    *
+"""
