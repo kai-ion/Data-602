92b4fac976200f8b4770710dabb60df7d211258a
kaiion <kaiion0509@gmail.com>
2024-10-01 12:37:56 -0400

    discussion week 10

---
+++ Discussion Question/discussion_week10.py
@@ -0,0 +1,67 @@
+"""Questions
+1. What is matplottlib and seaborn?  When would you choose one over the other?
+2. Image you are creating a visualization for a presentation at work.  What are some recommendations or guidelines you would follow to make engaging and informative visuals?
+3. Give an example of either a matplotlib or seaborn graphic (give code).  You may also reference an informative article.
+4. Read the following:  https://speedwell.com.au/en/insights/2019/the-manifesto-of-the-data-ink-ratio
+What is the data-ink ratio? 
+How can you use python libraries such as matplotlib, seaborn, plotly, etc. to incorporate this data-ink ratio in your visualizations?   
+"""
+
+"""Sample output
+
+"""
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q1. 
+"""
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q2.  
+"""
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q3. 
+
+"""
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q4. 
+
+"""
+
+###------------------------------------------------------------------------------------------------------  
+    
+def main():
+    # Q1 
+    print("###Q1### ")
+    print("")
+    print("\n")
+    
+    # Q2 
+    print("###Q2### ")
+    print("")
+    print("\n")
+    
+    # Q3 
+    print("###Q3### ")
+    
+    print("\n")
+    
+    # Q4 
+    print("###Q4### ")
+    
+    print("\n")
+    
+    
+
+if __name__ == "__main__":
+    main()
+
+"""
+*    *
+*  *
+* *
+* *
+*  *
+*    *
+"""
