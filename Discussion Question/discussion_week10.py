9f2dea0cc97bd3fbd818116ec5f46159283d14a8
kaiion <kaiion0509@gmail.com>
2024-10-01 12:42:30 -0400

    added questions 1 - 4

--- Discussion Question/discussion_week10.py
+++ Discussion Question/discussion_week10.py
@@ -12,44 +12,48 @@ How can you use python libraries such as matplotlib, seaborn, plotly, etc. to in
 """
 ###------------------------------------------------------------------------------------------------------ 
 """
-Q1. 
+Q1. What is matplottlib and seaborn?  When would you choose one over the other?
 """
 ###------------------------------------------------------------------------------------------------------ 
 """
-Q2.  
+Q2. Image you are creating a visualization for a presentation at work.  What are some recommendations or guidelines you would follow to make engaging and informative visuals?
 """
 ###------------------------------------------------------------------------------------------------------ 
 """
-Q3. 
+Q3. Give an example of either a matplotlib or seaborn graphic (give code).  You may also reference an informative article.
 
 """
 ###------------------------------------------------------------------------------------------------------ 
 """
-Q4. 
+Q4. Read the following:  https://speedwell.com.au/en/insights/2019/the-manifesto-of-the-data-ink-ratio
+What is the data-ink ratio? 
+How can you use python libraries such as matplotlib, seaborn, plotly, etc. to incorporate this data-ink ratio in your visualizations? 
 
 """
 
 ###------------------------------------------------------------------------------------------------------  
     
 def main():
-    # Q1 
-    print("###Q1### ")
+    # Q1 What is matplottlib and seaborn
+    print("###Q1### What is matplottlib and seaborn?  When would you choose one over the other?")
     print("")
     print("\n")
     
-    # Q2 
-    print("###Q2### ")
+    # Q2 guidelines and informative visuals
+    print("###Q2### Image you are creating a visualization for a presentation at work.  What are some recommendations or guidelines you would follow to make engaging and informative visuals?")
     print("")
     print("\n")
     
-    # Q3 
-    print("###Q3### ")
+    # Q3 example of either a matplotlib or seaborn graphic
+    print("###Q3### Give an example of either a matplotlib or seaborn graphic (give code).  You may also reference an informative article.")
     
     print("\n")
     
-    # Q4 
-    print("###Q4### ")
-    
+    # Q4  https://speedwell.com.au/en/insights/2019/the-manifesto-of-the-data-ink-ratio
+    print("###Q4### Read the following:  https://speedwell.com.au/en/insights/2019/the-manifesto-of-the-data-ink-ratio")
+    print("What is the data-ink ratio?")
+    print("How can you use python libraries such as matplotlib, seaborn, plotly, etc. to incorporate this data-ink ratio in your visualizations?")
+        
     print("\n")
     
     
