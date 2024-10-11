04c9ab0ab60987f1d28ff81d62e3769adafb355a
kaiion <kaiion0509@gmail.com>
2024-10-10 23:47:04 -0400

    updated discussion week 12

---
+++ Discussion Question/discussion_week12.py
@@ -0,0 +1,119 @@
+"""Questions
+1. What are the different types of machine learning? 
+2. What is the difference between supervised and unsupervised machine learning? When would you use one method over another?
+3. What is overfitting, how can you avoid it?
+4. What is 'training' and 'test' set in a machine learning model?
+5. How can you handle missing or corrupted data in a dataset? 
+6. What are some applications of supervised machine learning? 
+7. What is the KNN algorithm? (Write a brief description)
+8. What are advantaged of the KNN algorithm? 
+9. What are disadvantages of the KNN algorithm? 
+"""
+
+"""Sample output
+
+"""
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q1. What are the different types of machine learning? 
+"""
+
+    
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q2. What is the difference between supervised and unsupervised machine learning? When would you use one method over another?
+"""
+
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q3. What is overfitting, how can you avoid it?
+"""
+
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q4. What is 'training' and 'test' set in a machine learning model?
+"""
+
+
+###------------------------------------------------------------------------------------------------------  
+
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q5. How can you handle missing or corrupted data in a dataset?
+"""
+
+###------------------------------------------------------------------------------------------------------  
+
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q6. What are some applications of supervised machine learning? 
+"""
+
+###------------------------------------------------------------------------------------------------------  
+
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q7. What is the KNN algorithm? (Write a brief description)
+"""
+
+###------------------------------------------------------------------------------------------------------  
+
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q8. What are advantaged of the KNN algorithm?
+"""
+
+###------------------------------------------------------------------------------------------------------  
+
+###------------------------------------------------------------------------------------------------------ 
+"""
+Q9. What are disadvantages of the KNN algorithm? 
+"""
+
+###------------------------------------------------------------------------------------------------------  
+
+def main():
+    
+    
+    # Q1 Types of machine learning
+    print("###Q1### What are the different types of machine learning?   ")
+    
+    print("\n")
+    
+    # Q2 Supervised vs unsupervised ML
+    print("###Q2### What is the difference between supervised and unsupervised machine learning? When would you use one method over another?")
+    
+    print("\n")
+    
+    # Q3 Overfitting
+    print("###Q3### What is overfitting, how can you avoid it?")
+    
+    print("\n")
+    
+    # Q4  training and test set
+    print("###Q4### What is 'training' and 'test' set in a machine learning model?")
+    
+    print("\n")
+    
+    # Q5  Missing or corrupted data
+    print("###Q5### How can you handle missing or corrupted data in a dataset?")
+    
+    print("\n")
+    
+    # Q6  applications of supervised ML
+    print("###Q6### What are some applications of supervised machine learning? ")
+    
+    print("\n")
+    
+
+if __name__ == "__main__":
+    main()
+
+"""
+*    *
+*  *
+* *
+* *
+*  *
+*    *
+"""
